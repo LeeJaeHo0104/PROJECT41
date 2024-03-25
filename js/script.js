b2f951ae60975c24ec399073704ceb3d38e08b6d
Your Name <ljh2735294@naver.com>
2024-03-25 17:46:02 +0900

    footer 제외 css 완료

--- js/script.js
+++ js/script.js
@@ -15,14 +15,28 @@ setInterval(() => {
 	slideFn();
 }, 4000);
 
-//sec1 
+//스크롤 애니메이션
 window.onscroll = function() {
 	let height = window.pageYOffset;
 	console.log('스크롤 높이: ', height);
 	
+	//sec1 
 	const blurBox = document.querySelector('.blur_box');
-	if(height > 800){
+	if(height > 800 && height < 1700){
 		blurBox.style.width = '0rem';
+	}else{
+		blurBox.style.width = '20rem';
 	}
 
+	//sec4
+	const sec4Bg = document.querySelector('.sec4_bg');
+	if(height > 3350 && height < 4500){
+		sec4Bg.style.filter = 'blur(0px)';
+		sec4Bg.style.backgroundSize = '100% 100%';
+		sec4Bg.style.backgroundPosition = 'center';
+	}else{
+		sec4Bg.style.filter = 'blur(30px)';
+		sec4Bg.style.backgroundSize = '150% 150%';
+		sec4Bg.style.backgroundPosition = 'right top';
+	}
 }
